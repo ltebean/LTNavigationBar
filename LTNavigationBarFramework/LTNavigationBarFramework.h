bc59902215a97948163c22e1a1a342af4999f3cd
Justus Kandzi <justus.kandzi@gmail.com>
2017-02-09 12:49:27 +0100

    add carthage support

---
+++ LTNavigationBarFramework/LTNavigationBarFramework.h
@@ -0,0 +1,18 @@
+//
+//  LTNavigationBarFramework.h
+//  LTNavigationBarFramework
+//
+//  Created by Justus Kandzi on 09/02/2017.
+//  Copyright © 2017 ltebean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LTNavigationBarFramework.
+FOUNDATION_EXPORT double LTNavigationBarFrameworkVersionNumber;
+
+//! Project version string for LTNavigationBarFramework.
+FOUNDATION_EXPORT const unsigned char LTNavigationBarFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LTNavigationBarFramework/PublicHeader.h>
+#import "UINavigationBar+Awesome.h"
